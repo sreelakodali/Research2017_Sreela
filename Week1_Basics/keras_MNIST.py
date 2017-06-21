13369e5bf423cbd1abe48dbc9a173cd95e9333ac
Sreela <sreelakodali@gmail.com>
2017-06-21 13:42:37 -0400

    tested dropout and regularizers in mlp and finished cnn model

--- Week1_Basics/keras_MNIST.py
+++ Week1_Basics/keras_MNIST.py
@@ -1,13 +1,15 @@
 # Sreela Kodali - MNIST on Keras
-# June 20th, 2017  
+# June 21st, 2017  
 
 from __future__ import print_function                                          
 import keras
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Dropout
 from keras.datasets import mnist
 from keras.optimizers import RMSprop, SGD
+from keras.regularizers import l1, l2
 import numpy
+import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 numpy.random.seed(7)
 
@@ -15,7 +17,7 @@ numpy.random.seed(7)
 #load data
 (xtrain, ytrain), (xtest, ytest) = mnist.load_data()
 
-# extract sections of xtrain and xtest
+# reshape dataset from 28x28 images to sets of 784 pixels
 xtrain = xtrain.reshape(60000, 784)
 xtest = xtest.reshape(10000, 784)
 
@@ -35,59 +37,59 @@ print(xtest.shape[0], 'test samples')
 ytrain = keras.utils.to_categorical(ytrain, 10)
 ytest = keras.utils.to_categorical(ytest, 10)
 
-#print(xtrain.shape)
-#print(xtest.shape)
-#print(ytrain.shape)
-#print(ytest.shape)
 
-## CONTROL PARAMETERS
+# CONTROL PARAMETERS
 l_r = 0.5
 p = 0.6
 d = 0.0
 n_nodesL1 = 25
-n_nodesL2 = 12
+n_nodesL2 = 15
 n_nodesL3 = 10
+drate = 0.05
+reg = 0.01
+#kernel_regularizer=l2(reg))
 
+
+#creating sgd optimizer
 opt = SGD(lr=l_r, momentum=p, decay=d, nesterov=False)
 
 # Part 2: Define Model
 model = Sequential()
 model.add(Dense(n_nodesL1, input_shape=(784,),  activation='relu'))
-model.add(Dense(n_nodesL2, activation='relu'))
+model.add(Dropout(drate))
+model.add(Dense(n_nodesL2, activation='relu', bias_regularizer=l1(reg)))
+model.add(Dropout(drate))
 model.add(Dense(n_nodesL3, activation='sigmoid'))
 
 # Part 3: Compile Model
 model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
 
+#setting up variables for forloop to fit/evaluate models with different epochs
 epoch = [3, 6, 9, 12, 15]
-trainP = numpy.zeros(5)
-testP = numpy.zeros(5)
+trainP = numpy.zeros(len(epoch))
+testP = numpy.zeros(len(epoch))
 i = 0
-for ep in epoch:
-	# Fit Model
-	print('---EPOCH #:', ep, '---')
-	model.fit(xtrain, ytrain, epochs=ep, batch_size=10, verbose=0, validation_data=(xtest, ytest))
+
+#fit model and evaluate train and test performance for x epochs
+for x in epoch:
+	print('---EPOCH #:', x, '---')
+	model.fit(xtrain, ytrain, epochs=x, batch_size=10, verbose=0, validation_data=(xtest, ytest))
 	trainPerformance = model.evaluate(xtrain, ytrain, verbose=0)
-	print('Epoch #:', ep, 'Train loss:', trainPerformance[0])
-	print('Epoch #:', ep, 'Train accuracy:', trainPerformance[1])
+	#print('Epoch #:', x, 'Train loss:', trainPerformance[0])
+	print('Epoch #:', x, 'Train accuracy:', trainPerformance[1])
 	trainP[i] = trainPerformance[1]
 
 	testPerformance = model.evaluate(xtest, ytest, verbose=0)
-	print('Epoch #:', ep, 'Test loss:', testPerformance[0])
-	print('Epoch #:', ep, 'Test accuracy:', testPerformance[1])
+	#print('Epoch #:', x, 'Test loss:', testPerformance[0])
+	print('Epoch #:', x, 'Test accuracy:', testPerformance[1])
 	testP[i] = testPerformance[1]
 	i += 1
-fig = plt.figure()
-plt.plot(epoch, trainP, epoch, testP)
-plt.xlabel('Epoch')
-plt.ylabel('Accuracy')
-#plt.figlegend((trainP, testP), ('Train Accuracy', 'Test Accuracy'), 'upper right')
-plt.show()
 
-# Bonus: Predictions; inputting data and getting corresponding predictions                             
-# Note: in this scenario, using the same data
-   
-#predictions = model.predict(X)
-#round predictions                                                                                     
-#rounded = [round(x[0]) for x in predictions]
-#print(rounded)
+#plot train and test accuracy by # of epochs
+fig = plt.figure()
+line1, = plt.plot(epoch, trainP, label='Train')
+line2, = plt.plot(epoch, testP, label='Test')
+plt.xlabel('# of Epochs')
+plt.ylabel('Accuracy')
+plt.legend(handles=[line1, line2])
+plt.show()
--- Week1_Basics/keras_cnn_example.py
+++ Week1_Basics/keras_cnn_example.py
@@ -1,22 +1,66 @@
 # Sreela Kodali - First CNN on Keras
-# June 20th, 2017
+# June 21st, 2017
 
 import numpy as np
 np.random.seed(123)
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
-from keras.layers import Convolution2D, MaxPooling2D
+from keras.layers import Conv2D, MaxPooling2D
 from keras.utils import np_utils
 from keras.datasets import mnist
 
 # Loading image data from MNIST
 (xtrain, ytrain), (xtest, ytest) = mnist.load_data()
 
-#shape of dataset
-print(xtrain.shape)
-
 #display image as a test
 #from matplotlib import pyplot as plt
 #plt.show(xtrain[0])
 
-#Preprocess input data
+#Reshape input data with image's depth for theano
+xtrain = xtrain.reshape(xtrain.shape[0], 1, 28, 28)
+xtest = xtest.reshape(xtest.shape[0], 1, 28, 28)
+
+print(xtrain.shape)
+
+# casting each array
+xtrain = xtrain.astype('float32')
+xtest = xtest.astype('float32')
+
+# making array values from 0 to 1 
+xtrain /= 255
+xtest /= 255
+
+# converting class vectors to binary class matrices
+ytrain = np_utils.to_categorical(ytrain, 10)
+ytest = np_utils.to_categorical(ytest, 10)
+
+print(ytrain.shape)
+
+#dropping rates for convolution model and fully connected model
+drate1 = 0.25
+drate2 = 0.5
+
+#Setting up the Model, firs adding convolution layers and then fully connected
+model = Sequential()
+model.add(Conv2D(32, (3,3), activation='relu', data_format='channels_first', input_shape=(1,28,28)))
+print('Shape of current model output:', model.output_shape)
+model.add(Conv2D(32, (3,3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(drate1))
+
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(drate2))
+model.add(Dense(10, activation='softmax'))
+
+#compile the model
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+#fit model with training data
+model.fit(xtrain, ytrain, batch_size=32, epochs=10, verbose=1)
+
+#evaluate the performance
+performance = model.evaluate(xtrain, ytrain, verbose=1)
+print('Train accuracy:', performance[1])
+performance = model.evaluate(xtest, ytest, verbose=1)
+print('Test accuracy:', performance[1])
